4fc7d2b9507bd79b099cfc117a76bf2e6c8bca2c
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-08-19 19:44:46 -0300

    melhoria das funções no script, inserção do input e botao que trata do tamanho de pixel-board e correções lint

--- script.js
+++ script.js
@@ -1,91 +1,80 @@
-var cores = ["red", "blue", "yellow"];
-var contPixel = 0;
+const escolhas = document.getElementById('color-palette');
+const quadro = document.getElementById('pixel-board');
+let corSelecionada;
 
-var visualizar = document.getElementById('visualizar');
-var escolhas = document.getElementById('color-palette');
-var quadro = document.getElementById('pixel-board');
+function colorPalette(cor) { // cria o botao e coloca uma das cores nele
+  const setor = document.createElement('div');
+  if (cor === 'black') {
+    // alert('black');
+    setor.className = 'color selected';
+    corSelecionada = 'black'; // a primeira cor selecionada eh a preta
+  } else {
+    setor.className = 'color';
+  }
+  setor.value = cor;// define que o valor de determinado botao será o da cor dele cor, algo a ser passado para a função que escuta o click
+  setor.style.backgroundColor = cor;// cria o botao e coloca uma das cores nele
+  escolhas.appendChild(setor);// cria os varios botoes de acordo com a quantidade de cores do array
 
-colorPalette ("black");
-//https://www.horadecodar.com.br/2022/01/16/gerar-cor-aleatoria-com-javascript/
-for(let index = 0; index < 3; index += 1){
-	generateColor();
 }
+
+colorPalette('black');
 
 function generateColor() {
-	
-		
-		//alert("teste");
-	
-		const letters = '0123456789ABCDEF';
-		let color = '#';
-		
-		for (let i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-		}
-		
-		return colorPalette (color);
-}	
 
-function colorPalette(cor) {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
 
-	var setor = document.createElement('div');
-	if(cor == 'black'){
-	 	//alert('black');
-	 	setor.className = 'color selected';// cria o botao e coloca uma das cores nele
-	}else{
-		setor.className = 'color';
-	}
-	setor.value = cor;// define que o valor de determinado botao será o da cor dele cor, algo a ser passado para a função que escuta o click
-	setor.style.backgroundColor = cor;// cria o botao e coloca uma das cores nele
-	escolhas.appendChild(setor);// cria os varios botoes de acordo com a quantidade de cores do array
-		
+  for (let i = 0; i < 6; i += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return colorPalette(color);
 }
 
-for(let index2 = 0; index2 < 25; index2 += 1){
-	criaQuadro ("white");
-
+// https://www.horadecodar.com.br/2022/01/16/gerar-cor-aleatoria-com-javascript/
+for (let index = 0; index < 3; index += 1) {
+  generateColor();
 }
 
 function criaQuadro(cor) {
-	//alert("teste");
-
-	var pixel = document.createElement('div');
-	pixel.className = 'pixel';// cria o botao e coloca uma das cores nele
-	pixel.value = cor;// define que o valor de determinado botao será o da cor dele cor, algo a ser passado para a função que escuta o click
-	//parte.type = 'button';// define que a div buton será do tipo botão
-	pixel.style.backgroundColor = cor;// cria o botao e coloca uma das cores nele
-	quadro.appendChild(pixel);
+  // alert("teste");
+  const pixel = document.createElement('div');
+  pixel.className = 'pixel';// cria o botao e coloca uma das cores nele
+  pixel.value = cor;// define que o valor de determinado botao será o da cor dele cor, algo a ser passado para a função que escuta o click
+  pixel.style.backgroundColor = cor;// cria o botao e coloca uma das cores nele
+  quadro.appendChild(pixel);
 }
 
-escolhas.addEventListener('click', function(e){//seleciona a cor da paleta
-	//alert(click);
-  let filhos = e.target.parentElement.childNodes;// target retorna o nó de elementos que foi clicado
-  //alert(filhos.length);
-  for(let index = 0; index<filhos.length; index+=1){
-		//alert(filhos[index]);
-    	filhos[index].className = 'color';
+for (let index2 = 0; index2 < 25; index2 += 1) {
+  criaQuadro('white');
+}
+
+escolhas.addEventListener('click', (e) => { // seleciona a cor da paleta
+  // alert(click);
+  // const children = escolhas.children;
+  // https://www.javascripttutorial.net/javascript-dom/javascript-get-child-element/
+  const { children } = escolhas;
+  // alert(children.length);
+  for (let index = 0; index < children.length; index += 1) {
+    // alert(children[index]);
+    if (children[index] !== e.target) {
+      children[index].className = 'color';
+    } else {
+      children[index].className = 'color selected';
+
+      corSelecionada = children[index].style.backgroundColor;
+    }
   }
-  e.target.className = 'color selected';
 });
 
-
-document.querySelector('#pixel-board').addEventListener('click', function(e){
-	// pinta a div selecionada
- // alert("teste");
-  let corSelecionada = getComputedStyle(document.querySelector('.selected')).backgroundColor;
-  
-  e.target.style.backgroundColor = corSelecionada;
-  
+quadro.addEventListener('click', (e) => {
+  // pinta a div selecionada
+  // alert("teste");
+  e.target.style.backgroundColor = corSelecionada;// adiciona a cor selecionada ao quadro cliclado no pixel-board
 });
 
+const btn = document.querySelector('#clear-board');
 
-var btn = document.querySelector("#clear-board");
-
-btn.addEventListener("click", function() {
-    
-    location.reload();
-
+btn.addEventListener('click', () => {
+  window.location.reload();
 });
-
-window.onload
-
