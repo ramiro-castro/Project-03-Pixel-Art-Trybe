cc3314492df278335bc671d0b1c99c7264cab0b2
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-08-22 21:58:21 -0300

    concluindo os requisitos bonus

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const escolhas = document.getElementById('color-palette');
 const quadro = document.getElementById('pixel-board');
+const botaoAlterar = document.getElementById('generate-board');
 let corSelecionada;
+const newQuadro = document.createElement('div');
+// https://stackoverflow.com/questions/9422974/createelement-with-id
+newQuadro.setAttribute('id', 'pixel-board');
+let tamanho = 5;
 
-function colorPalette(cor) { // cria o botao e coloca uma das cores nele
+function colorPalette(cor) { // cria a div e coloca uma das cores nele
   const setor = document.createElement('div');
   if (cor === 'black') {
     // alert('black');
@@ -11,16 +16,14 @@ function colorPalette(cor) { // cria o botao e coloca uma das cores nele
   } else {
     setor.className = 'color';
   }
-  setor.value = cor;// define que o valor de determinado botao será o da cor dele cor, algo a ser passado para a função que escuta o click
-  setor.style.backgroundColor = cor;// cria o botao e coloca uma das cores nele
-  escolhas.appendChild(setor);// cria os varios botoes de acordo com a quantidade de cores do array
-
+  setor.value = cor;// define que o valor de determinada div será o da cor dele cor, algo a ser passado para a função que escuta o click
+  setor.style.backgroundColor = cor;// cria a div e coloca uma das cores nele
+  escolhas.appendChild(setor);// cria as varias divs de acordo com a quantidade de cores do array
 }
 
-colorPalette('black');
+colorPalette('black'); // primeira cor é preta
 
 function generateColor() {
-
   const letters = '0123456789ABCDEF';
   let color = '#';
 
@@ -39,14 +42,14 @@ for (let index = 0; index < 3; index += 1) {
 function criaQuadro(cor) {
   // alert("teste");
   const pixel = document.createElement('div');
-  pixel.className = 'pixel';// cria o botao e coloca uma das cores nele
-  pixel.value = cor;// define que o valor de determinado botao será o da cor dele cor, algo a ser passado para a função que escuta o click
-  pixel.style.backgroundColor = cor;// cria o botao e coloca uma das cores nele
+  pixel.className = 'pixel';// cria a classe da div
+  pixel.style.backgroundColor = cor;// coloca uma cor no fundo da div
   quadro.appendChild(pixel);
 }
 
-for (let index2 = 0; index2 < 25; index2 += 1) {
+for (let index2 = 0; index2 < tamanho * tamanho; index2 += 1) {
   criaQuadro('white');
+  // alert(index2);
 }
 
 escolhas.addEventListener('click', (e) => { // seleciona a cor da paleta
@@ -73,8 +76,39 @@ quadro.addEventListener('click', (e) => {
   e.target.style.backgroundColor = corSelecionada;// adiciona a cor selecionada ao quadro cliclado no pixel-board
 });
 
+newQuadro.addEventListener('click', (e) => {
+  // pinta a div selecionada
+  // alert("teste");
+  e.target.style.backgroundColor = corSelecionada;// adiciona a cor selecionada ao quadro cliclado no pixel-board
+});
+
 const btn = document.querySelector('#clear-board');
 
 btn.addEventListener('click', () => {
   window.location.reload();
+});
+
+function recriarQuadro(altura) {
+  for (let index2 = 0; index2 < (altura * altura); index2 += 1) {
+    criaQuadro('white');
+  }
+  quadro.style.width = `calc(${altura} * 54px)`; // https://stackoverflow.com/questions/45084227/using-css-calc-in-javascript-with-a-variable
+}
+
+// eslint-disable-next-line max-lines-per-function, complexity
+botaoAlterar.addEventListener('click', () => {
+  tamanho = parseInt(document.getElementById('board-size').value, 10);
+  // alert(tamanho);
+  quadro.innerText = '';// https://pt.stackoverflow.com/questions/441373/como-remover-todos-os-elementos-de-uma-div-em-javascript
+
+  if (tamanho >= 1) {
+    if (tamanho < 5) {
+      tamanho = 5;
+    } else if (tamanho > 50) {
+      tamanho = 50;
+    }
+    recriarQuadro(tamanho);
+  } else {
+    alert('Board inválido!');
+  }
 });
